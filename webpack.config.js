3e7083dcff0ced105ea3adb480399a523e782f29
Oscar Raig <oscar.raig@gmail.com>
2015-10-12 23:36:05 +0200

    optimizing-rebunding

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 var path = require('path');
-var config = {
-  entry: path.resolve(__dirname, 'app/main.js'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-    }]
-  }
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+
+config = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+    resolve: {
+        alias: {
+          'react': pathToReact
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel'
+        }],
+        noParse: [pathToReact]
+    }
 };
 
 module.exports = config;
